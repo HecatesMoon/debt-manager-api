cdbfe4d7a241ae6cc9f2d8529d522f4142d168ef
Joaquín Araya <88943020+HecatesMoon@users.noreply.github.com>
2026-03-24 21:16:59 -0300

    chore: clean imports in TestAuthController

--- src/main/java/com/hecatesmoon/expenses_manager/controller/TestAuthController.java
+++ src/main/java/com/hecatesmoon/expenses_manager/controller/TestAuthController.java
@@ -3,10 +3,6 @@ package com.hecatesmoon.expenses_manager.controller;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hecatesmoon.expenses_manager.dto.LoginRequest;
 import com.hecatesmoon.expenses_manager.dto.RegisterRequest;
 import com.hecatesmoon.expenses_manager.dto.UserResponse;
-import com.hecatesmoon.expenses_manager.model.User;
-import com.hecatesmoon.expenses_manager.security.JwtUtil;
 import com.hecatesmoon.expenses_manager.service.UsersService;
 
 import jakarta.validation.Valid;
